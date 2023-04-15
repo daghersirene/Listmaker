7be653463433374e09333e7a95f65324df660eea
Sirene Dagher <daghersirene@gmail.com>
2023-04-14 22:06:10 -0400

    Fix issues

--- app/src/main/java/com/raywenderlich/listmaker/ui/main/MainFragment.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ui/main/MainFragment.kt
@@ -1,24 +1,22 @@
 package com.raywenderlich.listmaker.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.raywenderlich.listmaker.R
 import com.raywenderlich.listmaker.databinding.FragmentMainBinding
 import com.raywenderlich.listmaker.models.TaskList
-import kotlin.properties.Delegates
 
 class MainFragment : Fragment(), ListSelectionRecyclerViewAdapter.ListSelectionRecyclerViewClickListener {
 
     interface MainFragmentInteractionListener {
         fun listItemTapped(list: TaskList)
     }
+
     lateinit var clickListener: MainFragmentInteractionListener
 
     private lateinit var binding: FragmentMainBinding
@@ -29,7 +27,11 @@ class MainFragment : Fragment(), ListSelectionRecyclerViewAdapter.ListSelectionR
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
 
         binding.listsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
@@ -39,8 +41,10 @@ class MainFragment : Fragment(), ListSelectionRecyclerViewAdapter.ListSelectionR
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity(),
-            MainViewModelFactory(PreferenceManager.getDefaultSharedPreferences(requireActivity())))
+        viewModel = ViewModelProvider(
+            requireActivity(),
+            MainViewModelFactory(PreferenceManager.getDefaultSharedPreferences(requireActivity()))
+        )
             .get(MainViewModel::class.java)
 
         val recyclerViewAdapter = ListSelectionRecyclerViewAdapter(viewModel.lists, this)
